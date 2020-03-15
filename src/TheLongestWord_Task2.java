3f5e9cf5e1d7f9cd4cc301e5f2e05f4c5f2cd4b7
LaurentDevec <vasilevartem91@yandex.ru>
2020-03-15 23:24:15 +0300

    LastVersion

--- src/TheLongestWord_Task2.java
+++ src/TheLongestWord_Task2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TheLongestWord {
+public class TheLongestWord_Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
